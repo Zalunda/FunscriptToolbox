0a28f06e3bafc203968de68ca022a4ded52483c4
zalunda <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>
2024-04-17 22:14:01 -0400

    Added little documentation in --FSTB-CreateSubtitles.bat to transform the script into a loop.

--- src/FunscriptToolbox/InstallationFiles/--FSTB-CreateSubtitles.bat
+++ src/FunscriptToolbox/InstallationFiles/--FSTB-CreateSubtitles.bat
@@ -1,13 +1,15 @@
 @echo off
-REM ScriptVersion:1.4
+REM ScriptVersion:1.5
 
 set "path=[[FunscriptToolboxFolder]];%path%"
 
+start:
 echo --- subtitles.create ---
 "FunscriptToolbox.exe" ^
 		subtitles.create ^
 		--config ".\--FSTB-SubtitleGeneratorConfig.json" ^
-		--sourcelanguage "ja" ^
 		--verbose ^
 		"*.mp4"
 pause
+REM Remove REM from the start of the next line to have a looping script (i.e. run tool, press space, run tool, ...)
+REM goto start:
