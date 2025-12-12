193e410b19841738a7926a0a03e22be966bb969d
zalunda <zalunda@hotmail.com>
2025-12-12 12:28:49 -0500

    Changed version to 2.0.17.

--- src/FunscriptToolbox/InstallationVerbs/VerbInstallation.cs
+++ src/FunscriptToolbox/InstallationVerbs/VerbInstallation.cs
@@ -34,7 +34,7 @@ namespace FunscriptToolbox.MotionVectorsVerbs
 
         public int Execute()
         {
-            var prefixExamples = "Default-2.0.16-";
+            var prefixExamples = "Default-2.0.17-";
 
             InstallOFSPlugin();
             CreateUseCaseFolder("FSTB-CreateSubtitles2025", "--FSTB-CreateSubtitles", ".bat", Resources.FSTB_CreateSubtitles_bat, UTF8NoBOM);
